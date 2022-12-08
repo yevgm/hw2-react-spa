2a8e09fa44717b2f8ff5f4d3c3f76009ca44bebe
yevgm <yevgeni.technion@gmail.com>
2022-12-08 14:08:45 +0200

    refactor and deploy to new branch

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-export function ShuffleArray(o:number[]) {
-    for(let j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-}
-
-export function NameToUpper(name:string){
-    return name.charAt(0).toUpperCase() + name.slice(1)
-}
